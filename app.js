c5389e80132c919d75fcbc7b2d0449c1e4d858db
JokeGp <73568212+KikeX3@users.noreply.github.com>
2022-04-04 18:41:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-app.set(`view engine`, `ejs`);
+app.set(`view engine`, `ejs`); // le pedimos a ejs que nos renderise 
 
 app.use(`/`, function (req, res, next) {
   console.log(`Request Url:` + req.url);
@@ -10,6 +10,6 @@ app.use(`/`, function (req, res, next) {
 
 app.get(`/number/:id`, function (req, res) {
   res.render(`number`, { ID: req.params.id });
-});
+});// hacemos una ruta normal como las de siempre en este caso cambiando el nombre person por number solo para simbolizar que nos daran un numero, manteniendo el parametro id
 
 app.listen(port);
